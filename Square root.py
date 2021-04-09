ad49d2227f3b7094ffd3f9f6ed81134ed537cdde
lite7lux <opu.noor@gmail.com>
2021-04-08 21:44:26 -0500

    square root program

---
+++ Square root.py
@@ -0,0 +1,11 @@
+import math
+#get input number
+number_string = input("Enter the number you need root:")
+print(number_string)
+number = float(number_string)
+
+#get square root and print output
+root = math.sqrt(number)
+print("The root is ; ",root)
+
+#End of program
